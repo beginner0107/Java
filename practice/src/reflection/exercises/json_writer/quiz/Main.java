dfa27e7ef4bc6a7722b9b4db851312a036116cc8
Beginner <dkstmdwn17@naver.com>
2024-04-28 23:50:28 +0900

    [07]객체 사이즈 계산 퀴즈 (미완성)

---
+++ practice/src/reflection/exercises/json_writer/quiz/Main.java
@@ -0,0 +1,11 @@
+package reflection.exercises.json_writer.quiz;
+
+// 객체의 크기에 대한 측정값은
+// SizeOf(accountSummary) = {header} + {reference} + SizeOf{firstName} +SizeOf{lastName} + SizeOf{age} + SizeOf{salary} =
+// 12 + 4 + (12 + 4 + 4) + (12 + 4 + 5) + 2 + 4 = 63
+public class Main {
+    public static void main(String[] args) {
+        AccountSummary accountSummary = new AccountSummary("John", "Smith", (short) 20, 100_000);
+
+    }
+}
