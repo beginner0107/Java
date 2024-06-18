4dd8b25866bf26e9bebdd828e22a8a1f0a93f211
Beginner <dkstmdwn17@naver.com>
2024-06-18 23:59:42 +0900

    [Java]분무기 객체 모델링

--- practice/src/syntax/training/ex02/WaterSpray.java
+++ practice/src/syntax/training/ex02/WaterSpray.java
@@ -4,12 +4,23 @@ package syntax.training.ex02;
  * 분무기 객체
  */
 public class WaterSpray {
+    private static final int CAPACITY = 200;
+
     private int remainingWaterInMl; // 현재 남아있는 물의 양
+
+    public void spray() {
+        this.remainingWaterInMl -= Math.min(this.remainingWaterInMl, 5);
+    }
 
     public int getRemainingWaterInMl() {
         return remainingWaterInMl;
     }
-    public void setRemainingWaterInMl(int remainingWaterInMl) {
-        this.remainingWaterInMl = remainingWaterInMl;
+    public void addWater(int amountInMl) {
+        this.remainingWaterInMl += amountInMl;
+        this.remainingWaterInMl = Math.min(remainingWaterInMl, amountInMl);
+    }
+
+    public void fillUp() {
+        this.remainingWaterInMl = CAPACITY;
     }
 }
